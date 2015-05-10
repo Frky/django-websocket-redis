b5ada868d941c6b9223c589bcb839ccb6d9de704
Franck De Goër <degoerdf@ensimag.fr>
2015-05-10 09:58:46 +0200

    Inlcude modifications on disconnection handling to WSGISocket

--- ws4redis/uwsgi_runserver.py
+++ ws4redis/uwsgi_runserver.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import uwsgi
+from datetime import datetime, timedelta 
 import gevent.select
 from ws4redis.exceptions import WebSocketError
 from ws4redis.wsgi_server import WebsocketWSGIServer
 
 
 class uWSGIWebsocket(object):
-    def __init__(self):
+    def __init__(self, user):
         self._closed = False
+        self._user = user
+        self._last_seen = None
 
     def get_file_descriptor(self):
         """Return the file descriptor for the given websocket"""
@@ -45,12 +48,24 @@ class uWSGIWebsocket(object):
 
     def close(self, code=1000, message=''):
         self._closed = True
+        try:
+            # If a handler has been defined in the settings, call it
+            # to notify disconnection of user
+            settings.DISCONNECTION_HANDLER(self.user)
+        except:
+            pass
+
+    def reset_last_seen(self):
+        self._last_seen = datetime.now()
+
+    def is_active(self):
+        return datetime.now() - self._last_seen <= timedelta(seconds=25)
 
 
 class uWSGIWebsocketServer(WebsocketWSGIServer):
-    def upgrade_websocket(self, environ, start_response):
+    def upgrade_websocket(self, environ, start_response, user):
         uwsgi.websocket_handshake(environ['HTTP_SEC_WEBSOCKET_KEY'], environ.get('HTTP_ORIGIN', ''))
-        return uWSGIWebsocket()
+        return uWSGIWebsocket(user)
 
     def select(self, rlist, wlist, xlist, timeout=None):
         return gevent.select.select(rlist, wlist, xlist, timeout)
